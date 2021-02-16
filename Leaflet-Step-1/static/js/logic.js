c4c3314230c01818518d95c4f95064565080e515
yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-02-15 21:43:17 -0800

    Created default popup with info.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,10 +1,10 @@
+// Create a map object
 var myMap = L.map("map", { 
     center: [37.0902, -95.7129], //I am using the US coordinates
     zoom: 4 
   }); 
   
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
+// Adding a tile layer (the background map image) to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -14,8 +14,20 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
+// Store our API endpoint inside url
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
 
+// Grab the data with d3
 d3.json(url, function(data){
-    console.log(data);
+    console.log(data.features);
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and magnitude of the earthquake
+    function createFeature(feature, layer){
+        layer.bindPopup(feature.properties.title + "<hr>"+ feature.properties.mag);
+    }
+    // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
+    var earthquakes = L.geoJSON(data.features, {
+    onEachFeature: createFeature
+  }).addTo(myMap);
 })
